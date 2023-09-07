db1340663958c37c65d8f99f0b4300f73371c405
Gabe <gabecoolkim@yahoo.com>
2023-09-06 19:14:38 -0500

    changes made to files

--- client/instagram-clone/src/App.jsx
+++ client/instagram-clone/src/App.jsx
@@ -1,36 +1,34 @@
 import "./App.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Login from "./components/Login";
+import Login from "./components/Login";
 import Home from "./components/Home";
 import Register from "./components/Signup";
-import LoginPage from './pages/Login';
-import SignupPage from './pages/Signup';
-import ProfilePage from "./pages/profile"; 
-
-
-
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import ProfilePage from "./pages/Profile";
+import Layout from "./components/Layout";
 
 function App() {
 	return (
-
 		<div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 			<div className="max-w-md w-full space-y-8">
+				
+				
 				<Routes>
-					<Route path="/" element={<LoginPage />} />
+					<Route path="/" element={<Layout />} />
 					<Route index element={<LoginPage />} />
 					<Route path="/signup" element={<SignupPage />} />
 					<Route path="/home" element={<Home />} />
-
+					<Route path="/Layout" element={<Layout />} />
 					<Route path="/profile" element={<ProfilePage />} />
-
-
+				
+					
 				</Routes>
-
+				
 				
 			</div>
 		</div>
-
 	);
 }
 
--- client/instagram-clone/src/pages/Profile.jsx
+++ client/instagram-clone/src/pages/Profile.jsx
@@ -1,9 +1,14 @@
+import Layout from "../components/Layout";
 import Profile from "../components/Profile";
+import { Outlet, Link } from "react-router-dom";;
+// import Layout from "./Layout";
 
 const ProfilePage = () => {
 
   return (
-    <Profile />
+    <Layout />,
+    <Profile />,
+    <Outlet/>
   )
 
 }
